a676e428e2a56dd4641ea7f9c234b6f5d2d8a3e5
cator <carladam.tornkvist@student.kyh.se>
2020-08-31 13:49:28 +0200

    asggagadg

---
+++ main.py
@@ -0,0 +1,3 @@
+print("Carl-Adam Törnkvist. Växte upp i Karlshamn Blekinge.")
+print("Har tidigare erfarenhet i JAVA men allt för länge sedan. På Teknikprogrammet gymnasiet 2009ish.")
+print("Har sökt PVT för att skaffa mig en karriär inom IT.")
