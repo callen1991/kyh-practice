9ac8ede5fe1129895dd4018e7bbd27910041f649
cator <carladam.tornkvist@student.kyh.se>
2020-09-01 11:36:32 +0200

    whatsyournumber_språkbyte1.py

--- UppgiftDag2.py
+++ UppgiftDag2.py
@@ -1,24 +1,25 @@
 import random
 
+antal_gissningar = 0
 n = random.randint(1, 100)
 print("Jag tänker på en siffra mellan 1 och 100. Gissa vilken?")
 
-while True:
-    text = input("Din gissning: ")
-    as_number = int(text)
 
-    if as_number == n:
-        print("Korrekt!")
-        break
+def mainloop(antal_gissningar, n):
+    while True:
+        text = input("Din gissning: ")
+        as_number = int(text)
+        antal_gissningar = antal_gissningar +1
+        if as_number == n:
+            print("Korrekt! " + str(antal_gissningar) + " gissningar använda")
+            break
 
-    if as_number < n:
-     print("Fel, mitt nummer är högre... Försök igen!")
+        if as_number < n:
+            print("Fel, mitt nummer är högre... Försök igen!")
 
-    if as_number > n:
-     print("Fel, mitt nummer är lägre... Försök igen!")
-
-
-
-
+        if as_number > n:
+            print("Fel, mitt nummer är lägre... Försök igen!")
 
 
+
+mainloop(antal_gissningar, n)
