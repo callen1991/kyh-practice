21448a84d12d70e63ab7330a9183c35a95b895bf
cator <carladam.tornkvist@student.kyh.se>
2020-09-02 09:25:30 +0200

    SubOndag1

---
+++ LektionOnsdag.py
@@ -0,0 +1,44 @@
+def add(a, b):
+
+
+    result = a + b
+    return result
+
+
+def subtract(a, b):
+
+
+    result = a + b
+    return result
+
+
+def multiply(a, b):
+
+
+    result = a + b
+    return result
+
+
+def divide(a, b):
+
+
+    result = a + b
+    return result
+
+
+def run():
+
+
+    print("This program will help with simple calculations. What do you want to do?")
+    print("1 - add numbers")
+    print("2 - subtract numbers")
+answer = input(">> ")
+a = int(input("A="))
+b = int(input("B="))
+if answer == "1":
+    result = add(a, b)
+if answer == "2":
+    result = subtract(a, b)
+print("Result = " + str(result))
+if __name__ == '__main__':
+    run()
--- UppgiftDag2.py
+++ UppgiftDag2.py
@@ -5,21 +5,28 @@ n = random.randint(1, 100)
 print("Jag tänker på en siffra mellan 1 och 100. Gissa vilken?")
 
 
+
+
+
+
+
 def mainloop(antal_gissningar, n):
     while True:
         text = input("Din gissning: ")
         as_number = int(text)
-        antal_gissningar = antal_gissningar +1
+        antal_gissningar = antal_gissningar + 1
         if as_number == n:
             print("Korrekt! " + str(antal_gissningar) + " gissningar använda")
             break
 
         if as_number < n:
             print("Fel, mitt nummer är högre... Försök igen!")
+            print("du har gjort " + str(max_guesses) + " gissningar använda")
 
         if as_number > n:
             print("Fel, mitt nummer är lägre... Försök igen!")
+            print("du har gjort " + str(max_guesses) + " gissningar använda")
 
-
-
+ask_number()
 mainloop(antal_gissningar, n)
+
