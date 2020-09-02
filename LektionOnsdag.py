0aab0b3ecd67b73a285efbd1bb6b11d33a1d7704
cator <carladam.tornkvist@student.kyh.se>
2020-09-02 09:34:57 +0200

    SubOndag2

--- LektionOnsdag.py
+++ LektionOnsdag.py
@@ -32,13 +32,13 @@ def run():
     print("This program will help with simple calculations. What do you want to do?")
     print("1 - add numbers")
     print("2 - subtract numbers")
-answer = input(">> ")
-a = int(input("A="))
-b = int(input("B="))
-if answer == "1":
-    result = add(a, b)
-if answer == "2":
-    result = subtract(a, b)
-print("Result = " + str(result))
+    answer = input(">> ")
+    a = int(input("A="))
+    b = int(input("B="))
+    if answer == "1":
+        result = add(a, b)
+    if answer == "2":
+        result = subtract(a, b)
+    print("Result = " + str(result))
 if __name__ == '__main__':
     run()
